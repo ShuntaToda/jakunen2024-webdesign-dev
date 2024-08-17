843d7e0706423b11ad859c5654099c4b46145262
ShuntaToda <todashuntawork@gmail.com>
2024-08-17 18:49:27 +0900

    copy template

--- src/workspace/src/App.tsx
+++ src/workspace/src/App.tsx
@@ -1,9 +1,72 @@
-
-
 function App() {
 
   return (
     <>
+      <div className="container">
+        <div className="score">00012340</div>
+        <div className="field">
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile enemy"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile flower"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile player"></div>
+          <div className="tile mushroom"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile stump"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile flower"></div>
+          <div className="tile"></div>
+          <div className="tile stump"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile stump"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile mushroom"></div>
+          <div className="tile"></div>
+          <div className="tile rock"></div>
+          <div className="tile rock"></div>
+          <div className="tile"></div>
+          <div className="tile stump"></div>
+          <div className="tile flower"></div>
+          <div className="tile rock"></div>
+        </div>
+      </div>
     </>
   )
 }
