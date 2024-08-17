282111674e8ca3ba022af0ada97e48f2f035a1c0
ShuntaToda <todashuntawork@gmail.com>
2024-08-17 19:09:31 +0900

    add result template

--- src/workspace/src/components/Result.tsx
+++ src/workspace/src/components/Result.tsx
@@ -1,5 +1,31 @@
 export const Result = () => {
   return (
-    <div>Result</div>
+    <>
+      <h1 className="heading">GAME OVER</h1>
+      <section className="section score">
+        <h2 className="score__heading">今回のスコア</h2>
+        <div className="score__value">00012340</div>
+        <p className="score__text">ニックネーム求む</p>
+        <button className="score__btn">スコア投稿</button>
+      </section>
+      <div className="divider"></div>
+      <section className="section ranking">
+        <h2 className="ranking__heading">ランキング</h2>
+        <div className="ranking-row">
+          <div className="ranking__name">Player1's Name</div>
+          <div className="ranking__score">00012340</div>
+        </div>
+        <div className="ranking-row">
+          <div className="ranking__name">Player2's Name</div>
+          <div className="ranking__score">00012340</div>
+        </div>
+        <div className="ranking-row">
+          <div className="ranking__name">Player3's Name</div>
+          <div className="ranking__score">00012340</div>
+        </div>
+      </section>
+      <div className="divider"></div>
+      <a className="replay" href="./">リプレイ</a>
+    </>
   )
 }
