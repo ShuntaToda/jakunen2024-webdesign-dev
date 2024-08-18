5d6449debb1525e0fcde5c2bbe21738842ab569b
ShuntaToda <todashuntawork@gmail.com>
2024-08-18 09:54:33 +0900

    add cell selector

--- src/workspace/src/components/Game.tsx
+++ src/workspace/src/components/Game.tsx
@@ -1,4 +1,4 @@
-import { Field } from "../types/field"
+import { CellValue, Field } from "../types/field"
 import { Route } from "../types/route"
 
 interface GameProps {
@@ -9,70 +9,31 @@ interface GameProps {
 }
 
 export const Game: React.FC<GameProps> = ({ setRoute, field, setScore, score }) => {
+
+  const selectCellType = (type: CellValue) => {
+    switch (type) {
+      case 0:
+        return <div className="tile"></div>;
+      case 1:
+        return <div className="tile rock"></div>;
+      case 2:
+        return <div className="tile flower"></div>;
+      case 3:
+        return <div className="tile mushroom"></div>;
+    }
+  }
   return (
     <>
       <div className="score">{score.toString().padStart(8, '0')}</div>
       <div className="field">
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile enemy"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile flower"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile player"></div>
-        <div className="tile mushroom"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
+        {field.map(row => {
+          return row.map(cell => {
+            return selectCellType(cell)
+          })
+        })}
+        {/* <div className="tile enemy"></div>
         <div className="tile stump"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile flower"></div>
-        <div className="tile"></div>
-        <div className="tile stump"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile stump"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile mushroom"></div>
-        <div className="tile"></div>
-        <div className="tile rock"></div>
-        <div className="tile rock"></div>
-        <div className="tile"></div>
-        <div className="tile stump"></div>
-        <div className="tile flower"></div>
-        <div className="tile rock"></div>
+        <div className="tile player"></div> */}
       </div>
     </>
   )
